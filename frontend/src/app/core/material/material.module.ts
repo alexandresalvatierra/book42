3a5647e16afef671d1f6ac3fc8728d8f9568fabc
Alexandre Salvatierra <alexandre.salvatierra@aodigital.com.br>
2022-09-10 18:10:40 -0300

    feat: add material

---
+++ frontend/src/app/core/material/material.module.ts
@@ -0,0 +1,50 @@
+import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatDialogModule } from '@angular/material/dialog';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatTableModule } from '@angular/material/table';
+
+const MODULES = [
+  MatToolbarModule,
+  MatMenuModule,
+  MatButtonModule,
+  MatSelectModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatExpansionModule,
+  MatSlideToggleModule,
+  MatInputModule,
+  MatCheckboxModule,
+  MatRadioModule,
+  MatDialogModule,
+  ReactiveFormsModule,
+  FormsModule,
+  MatProgressSpinnerModule,
+  MatSnackBarModule,
+  MatBadgeModule,
+  MatSidenavModule,
+  MatTooltipModule,
+  MatDatepickerModule,
+  MatTableModule,
+];
+
+@NgModule({
+  exports: [MODULES],
+})
+export class MaterialModule {}
