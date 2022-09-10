5d62cb559a6d31f264c0e448566c8945d969f879
Alexandre Salvatierra <alexandre.salvatierra@aodigital.com.br>
2022-09-10 15:17:16 -0300

    feat: add getters and setters to book entity

--- backend/src/main/java/com/book42/backend/entities/Book.java
+++ backend/src/main/java/com/book42/backend/entities/Book.java
@@ -15,9 +15,36 @@ public class Book {
   private String author;
   private LocalDateTime created_at;
 
-  public Book(String title, String author, LocalDateTime created_at) {
+  public String getId() {
+    return this.id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return this.title;
+  }
+
+  public void setTitle(String title) {
     this.title = title;
+  }
+
+  public String getAuthor() {
+    return this.author;
+  }
+
+  public void setAuthor(String author) {
     this.author = author;
+  }
+
+  public LocalDateTime getCreated_at() {
+    return this.created_at;
+  }
+
+  public void setCreated_at(LocalDateTime created_at) {
     this.created_at = created_at;
   }
+  
 }
